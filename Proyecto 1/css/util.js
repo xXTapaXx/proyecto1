f8166ad0caca966f343b426c65b36c2abfff6e1d
xXTapaXx <jgs14@hotmail.com>
2014-06-16 13:51:30 -0600

    Avance del Proyecto
    alerta de php para Agregar, Editar y Eliminar

--- Proyecto 1/css/util.js
+++ Proyecto 1/css/util.js
@@ -1,14 +1,73 @@
-function msg(){
-    var r=confirm("Esta Seguro?");
+//Agregar, Editar y Eliminar Carrera
+function agregarCarrera(){
+ 	alert("Carrera Agregada Existosamente");
+ }
+
+ function editarCarrera(){
+ 	alert("Carrera Editada Existosamente");
+ }
+
+function eliminarCarrera(){
+    var r=confirm("¿Esta Seguro que desea eliminar esta Carrera?");
 
     if(r==true){
-      alert("Usuario Eliminado");
+      alert("Carrera Eliminada Existosamente");
       windows.location="alert.html";
     }
     else{
       windows.location="alert.html";
     }
   }
+
+//Agregar, Editar y Eliminar Estudiante 
+function agregarEstudiante(){
+ 	alert("Estudiante Agregado Existosamente");
+ } 
+
+ function editarEstudiante(){
+ 	alert("Estudiante Editado Existosamente");
+ }
+
+function eliminarEstudiante(){
+    var r=confirm("¿Esta Seguro que desea eliminar este Estudiante?");
+
+    if(r==true){
+      alert("Estudiante Eliminado Existosamente");
+      windows.location="alert.html";
+    }
+    else{
+      windows.location="alert.html";
+    }
+  }
+
+//Agregar, Editar y Eliminar Usuario
+function agregarUsuario(){
+ 	alert("Usuario Agregado Existosamente");
+ }
+
+ function editarUsuario(){
+ 	alert("Usuario Editado Existosamente");
+ }
+
+function eliminarUsuario(){
+    var r=confirm("¿Esta Seguro que desea eliminar este Usuario?");
+
+    if(r==true){
+      alert("Usuario Eliminado Existosamente");
+      windows.location="alert.html";
+    }
+    else{
+      windows.location="alert.html";
+    }
+  }
+
+
+
+
+
+
+
+
 
 
 
