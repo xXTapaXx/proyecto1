ccfcd24511e59a01c4c006c419553b27106857b8
xXTapaXx <jgs14@hotmail.com>
2014-06-16 13:30:32 -0600

    Avance del Proyecto
    Creación de agregar y editar Carrera ,Estudiante y Usuario

---
+++ Proyecto 1/css/util.js
@@ -0,0 +1,14 @@
+function msg(){
+    var r=confirm("Esta Seguro?");
+
+    if(r==true){
+      alert("Usuario Eliminado");
+      windows.location="alert.html";
+    }
+    else{
+      windows.location="alert.html";
+    }
+  }
+
+
+
